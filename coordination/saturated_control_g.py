7911f6eb73be3f49b516ff94bb1db8a241ed4ceb
xypeng12 <xypeng@ucdavis.edu>
2025-08-11 22:57:53 -0700

    Update saturated_control_g.py

--- coordination/saturated_control_g.py
+++ coordination/saturated_control_g.py
@@ -6,9 +6,8 @@ from pulp import PULP_CBC_CMD
 from coordination.saturated_control_cycle import check_solution_status
 
 def saturated_control_green(n, t_T, L, l_initial, f, qs, g_max, g_min, q_branch_max, q_branch_min, q_in_1, h, M, z, lane_num_1, lane_num):
-    # 创建并求解问题
     problem = saturated_control_model_green(n, t_T, L, l_initial, f, qs, g_max, g_min,q_branch_max, q_branch_min, q_in_1, h, M, z, lane_num_1, lane_num)
-    solver = PULP_CBC_CMD(msg=True, gapRel = 0.05, timeLimit = 60)  #修改
+    solver = PULP_CBC_CMD(msg=True, gapRel = 0.05, timeLimit = 60)  
     problem.solve(solver)
     check_solution_status(problem)
 
@@ -17,22 +16,18 @@ def saturated_control_green(n, t_T, L, l_initial, f, qs, g_max, g_min, q_branch_
     return g, l, q_out, q_branch
 
 def saturated_control_model_green(n, t_T, L, l_initial, f, qs, g_max, g_min,q_branch_max,q_branch_min, q_in_1, h, M, z, lane_num_1, lane_num):
-    # 创建问题
     prob = pulp.LpProblem("Maximize_Outflow", pulp.LpMaximize)
 
-    # 定义变量
     q_out = pulp.LpVariable.dicts("q_out", ((i, t) for i in range(n) for t in range(t_T)), lowBound=0)
     x = pulp.LpVariable.dicts("x", ((i, t) for i in range(n) for t in range(t_T)), cat='Binary')
     q_branch = pulp.LpVariable.dicts("q_branch", (i for i in range(n)), lowBound=0)
     l = pulp.LpVariable.dicts("l", ((i, t) for i in range(n) for t in range(t_T)), lowBound=0)
-    # g = pulp.LpVariable.dicts("g", ((i, t) for i in range(n) for t in range(t_T)), lowBound=0)
 
     g = {}
     for i in range(n):
         for t in range(t_T):
             g[i, t] = pulp.LpVariable(f"g_{i}_{t}", lowBound=g_min[i], upBound=g_max[i])
 
-    # 初始条件
     for i in range(n):
         prob += l[i, 0] == l_initial[i], f"initial_l_{i}"
 
@@ -41,7 +36,6 @@ def saturated_control_model_green(n, t_T, L, l_initial, f, qs, g_max, g_min,q_br
     else:
         print('first_intersection_will_not_increase_queue')
 
-    # 添加约束
     for i in range(n):
         for t in range(t_T):
             if i == 0:
@@ -68,34 +62,30 @@ def saturated_control_model_green(n, t_T, L, l_initial, f, qs, g_max, g_min,q_br
                 prob += l[i, t + 1] <= l[i, t] + (q_in_i_t * 1/lane_num[i] + q_branch[i] * 1/lane_num[i] - g[i, t] * qs[i]) * (1/z) + M * (1 - x[i, t]), f"li_upper_bound_{i}_{t}"
                 prob += l[i, t + 1] <= M * x[i, t], f"li_upper_bound_2_{i}_{t}"
 
-            #prob += g_min[i] <= g[i, t] <= g_max[i], f"g_bounds_{i}_{t}"
 
         prob += q_branch[i] <= q_branch_max[i], f"q_branch_max_constraint_{i}"
         prob += q_branch[i] >= q_branch_min[i], f"q_branch_min_constraint_{i}"
 
-    # 目标函数
     prob += pulp.lpSum(q_out[i, t] for i in range(n) for t in range(t_T))
 
     return prob
 def read_solution(problem,n,t_T):
-    # 初始化列表
     g_values = [[0 for _ in range(t_T)] for _ in range(n)]
     l_values = [[0 for _ in range(t_T)] for _ in range(n)]
     q_out_values = [[0 for _ in range(t_T)] for _ in range(n)]
     q_branch_values = [0 for _ in range(n)]
     x_values = [[0 for _ in range(t_T)] for _ in range(n)]
 
-    # 填充列表
     for v in problem.variables():
         name_parts = v.name.split('_')
-        var_type = name_parts[0]  # 变量类型，如 'g', 'l', 'q_out', 'q_branch', 'x'
+        var_type = name_parts[0]  
         if var_type=='q':
             var_type=name_parts[0]+'_'+name_parts[1]
 
         if var_type in ['g', 'l', 'q_out', 'x']:
             indices = re.findall(r'\d+', v.name)
-            i = int(indices[0])  # 索引 i
-            t = int(indices[1])  # 时间 t
+            i = int(indices[0])  
+            t = int(indices[1])  
 
             if var_type == 'g':
                 g_values[i][t] = v.varValue
@@ -106,8 +96,7 @@ def read_solution(problem,n,t_T):
             elif var_type == 'x':
                 x_values[i][t] = v.varValue
         elif var_type == 'q_branch':
-            # 对于 'q_branch' 类型，变量名的格式为 'q_branch_i'
-            i = int(name_parts[2])  # 索引 i
+            i = int(name_parts[2])  
             q_branch_values[i] = v.varValue
     return g_values, l_values, q_out_values, q_branch_values, x_values
 
@@ -126,9 +115,9 @@ if __name__ == '__main__':
     z = 0.01
     g, l, q_out, q_branch, x = saturated_control_green(n, t_T, L, l_initial, f, qs, g_max, g_min, q_in_1, h, M, z)
 
-    # 打印列表
     print("g_values:", g)
     print("l_values:", l)
     print("q_out_values:", q_out)
     print("q_branch_values:", q_branch)
     print("x_values:", x)
+
